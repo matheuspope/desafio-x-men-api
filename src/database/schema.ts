07345c48e564602e8703759971058a130ce2380e
Matheus Pope <matheus.pope1@gmail.com>
2026-06-12 18:00:37 -0300

    Organizing schema.ts

--- src/database/schema.ts
+++ src/database/schema.ts
@@ -1,28 +1,32 @@
 import { timestamp } from "drizzle-orm/pg-core";
-import { pgTable, uuid, text, } from "drizzle-orm/pg-core";
-import {uniqueIndex} from"drizzle-orm/pg-core"
+import { pgTable, uuid, text } from "drizzle-orm/pg-core";
+import { uniqueIndex } from "drizzle-orm/pg-core";
 
-
-
-export const discoveredMutants = pgTable('discoveredMutants', {
+export const discoveredMutants = pgTable("discoveredMutants", {
   id: uuid().primaryKey().defaultRandom(),
   name: text().notNull(),
   codename: text().notNull(),
   power: text().notNull(),
   threatLevel: text().notNull(),
-})
+});
 
-export const teams = pgTable('teams', {
+export const teams = pgTable("teams", {
   id: uuid().primaryKey().defaultRandom(),
   name: text().notNull(),
   description: text(),
-})
+});
 
-export const mutants_Teams = pgTable('mutants_Teams', {
-   id: uuid().primaryKey().defaultRandom(),
-  discoveredMutantsId: uuid().notNull().references(() => discoveredMutants.id),
-  teamsId: uuid().notNull().references(() => teams.id),
-  createdAt: timestamp({withTimezone: true}).notNull().defaultNow()
-}, table =>[
-  uniqueIndex().on(table.discoveredMutantsId, table.teamsId)
-])
+export const mutants_Teams = pgTable(
+  "mutants_Teams",
+  {
+    id: uuid().primaryKey().defaultRandom(),
+    discoveredMutantsId: uuid()
+      .notNull()
+      .references(() => discoveredMutants.id),
+    teamsId: uuid()
+      .notNull()
+      .references(() => teams.id),
+    createdAt: timestamp({ withTimezone: true }).notNull().defaultNow(),
+  },
+  (table) => [uniqueIndex().on(table.discoveredMutantsId, table.teamsId)],
+);
