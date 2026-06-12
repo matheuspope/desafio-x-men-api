6eb693a82d7b7f2b0b9a8eed84f826e884dd969a
Matheus Pope <matheus.pope1@gmail.com>
2026-06-12 17:58:45 -0300

    add discoveringMutants ending point

--- server.ts
+++ server.ts
@@ -1,10 +1,27 @@
+import { fastify } from "fastify";
+import { db } from "./src/database/client";
+import { discoveredMutants } from "./src/database/schema";
 
-const fastify = require('fastify')
+const server =  fastify();
 
-const server = fastify()
+server.get("/discoveredMutants", async (request, reply) => {
+  const mutants = await db
+    .select({
+      id: discoveredMutants.id,
+      name: discoveredMutants.name,
+      codename: discoveredMutants.codename,
+      power: discoveredMutants.power,
+      threatLevel: discoveredMutants.threatLevel,
+    })
+    .from(discoveredMutants);
 
+  console.log(mutants);
+        return reply.send({mutants})
 
+});
 
-server.listen({port: 3333}).then(() => {
-  console.log("HTTP is running!")
-})
+server.post("/discoveredMutants", () => {});
+
+server.listen({ port: 3333 }).then(() => {
+  console.log("HTTP is running!");
+});
