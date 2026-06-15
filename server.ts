dc67a05c35b596af80a0ecb8bad53a0121dd3839
Matheus Pope <matheus.pope1@gmail.com>
2026-06-15 14:13:48 -0300

    adding ending point fo the post

--- server.ts
+++ server.ts
@@ -2,6 +2,8 @@ import { fastify } from "fastify";
 import { db } from "./src/database/client";
 import { discoveredMutants } from "./src/database/schema";
 
+
+
 const server =  fastify();
 
 server.get("/discoveredMutants", async (request, reply) => {
@@ -20,7 +22,28 @@ server.get("/discoveredMutants", async (request, reply) => {
 
 });
 
-server.post("/discoveredMutants", () => {});
+server.post("/discoveredMutants", async (request, reply) => {
+  const { name, codename, power, threatLevel} = request.body as {
+    name: string,
+    codename: string,
+    power: string,
+    threatLevel: string
+  }
+
+  const newMutants = await db
+  .insert(discoveredMutants)
+  .values({
+    name,
+    codename,
+    power,
+    threatLevel,
+  }).returning()
+  
+
+ return reply.status(201).send({discoveredMutants: newMutants[0].id})
+});
+
+
 
 server.listen({ port: 3333 }).then(() => {
   console.log("HTTP is running!");
